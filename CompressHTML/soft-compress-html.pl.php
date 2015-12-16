f5387cb83568f54e0ca2bedec6ee36f4553f0056
ILYA ERSHOV <ershov.ilya@gmail.com>
2015-12-16 11:59:48 +0300

    remove all HTML comments

--- CompressHTML/soft-compress-html.pl.php
+++ CompressHTML/soft-compress-html.pl.php
@@ -5,7 +5,7 @@
  * MODX event: OnWebPagePrerender
  * @var modX $modx
  */
-$arr = array("     ", "    ", "   ", "  ", " ", "\t");
+$arr = array("     ", "    ", "   ", "  ", " ", "\t"); //, "\r", "\n", "\r\n"
 function clear_output($buffer)
 {
     $search = array('/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s');
@@ -22,6 +22,7 @@ switch ($e->name)
         $content = clear_output($content);
         $content = str_replace("\r\n\r\n", "\r\n", $content);
         $content = str_replace($arr, " ", $content);
+        $content=preg_replace('/\<!-- .*--\>/U','',$content);
         $modx->resource->_output = $content;
     }
         break;
